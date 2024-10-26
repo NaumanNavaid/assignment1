c928ad512befe87d2a6c13d6a740b0b8a2678f34
Nauman-Navaid <naumannavaid378@gmail.com>
2024-10-26 22:04:09 +0500

    Update page.tsx

---
+++ src/app/service/page.tsx
@@ -0,0 +1,3 @@
+export default function service(){
+    return <p>I am an commerce student exploring different paths i can help in maintaining your accounting records in addition i can help in learning of html and css </p>
+}
